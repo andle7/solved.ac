fa70b7919cba047c768ef8a0db322c31f6ce222a
andle7 <hb1809@naver.com>
2023-06-08 23:11:03 +0900

    [Add] p2741

---
+++ Class1/2741.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = 0;
+		
+		N = in.nextInt();
+		
+		for(int i = 1; i < N+1; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+}
