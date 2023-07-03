d24631cd6e6f4406d8439c0f9beb8145b944917c
andle7 <hb1809@naver.com>
2023-07-03 20:16:52 +0900

    [Add] p2748

---
+++ Others/2748.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(fib(N));
+    }
+
+    public static long fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        long[] fibo = new long[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+
+        return fibo[n];
+    }
+}
